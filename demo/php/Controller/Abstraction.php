92aa72caa4c39644ca5c3f5e0f554e0a4af6a5f6
Hylent <hylent@qq.com>
2017-03-20 23:52:31 +0800

    Add queue consumer demo

--- demo/php/Controller/Abstraction.php
+++ demo/php/Controller/Abstraction.php
@@ -10,6 +10,14 @@ abstract class Abstraction extends \Nc\Mvc\ControllerDefault
 
         $this->injector->setClassNamespace('commonModelRepo', 'Demo\\Model');
         $this->injector->setClassNamespace('cliModelRepo', 'Demo\\Model\\Cli');
+
+        $this->injector['redis'] = function(\Nc\Di\Injector $injector) {
+            $h = $injector->config->get('app.redis.host', '127.0.0.1');
+            $p = $injector->config->get('app.redis.port', 6379);
+            $r = new \Redis();
+            $r->connect($h, $p);
+            return $r;
+        };
     }
 
     protected function onException(\Exception $ex)
